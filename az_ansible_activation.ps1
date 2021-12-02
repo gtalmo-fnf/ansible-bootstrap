227d0fbcec8c58c89091d755732b638c367cd378
gtalmo-fnf <93397594+gtalmo-fnf@users.noreply.github.com>
2021-12-02 09:26:54 -0600

    added az bootstrap script

---
+++ az_ansible_activation.ps1
@@ -0,0 +1,20 @@
+# New-LocalUser ansible -Password (ConvertTo-SecureString -AsPlainText -Force '<PASSWD>'); 
+
+# Add-LocalGroupMember -Group "Administrators" -Member ansible
+
+Enable-WSManCredSSP -Role Server -Force
+
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+
+powershell.exe -ExecutionPolicy ByPass -File $file -EnableCredSSP -DisableBasicAuth -ForceNewSSLCert -CertValidityDays 3650
+
+Remove-Item $file
+
+winrm delete winrm/config/Listener?Address=*+Transport=HTTP
+
+winrm enumerate winrm/config/Listener
+
