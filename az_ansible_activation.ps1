fec375058c352d35cca65d9d97acb2b579d29a24
gtalmo-fnf <93397594+gtalmo-fnf@users.noreply.github.com>
2021-12-02 13:34:03 -0600

    update path

--- az_ansible_activation.ps1
+++ az_ansible_activation.ps1
@@ -6,7 +6,7 @@ Enable-WSManCredSSP -Role Server -Force
 
 [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
 $url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
-$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+$file = "C:\ConfigureRemotingForAnsible.ps1"
 
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
 
